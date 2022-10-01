d269a813288860a68d6599810f434e1ed8311918
tux-box <nathan.flow@gmail.com>
2022-09-30 21:22:11 -0500

    fixed folder structure

--- scripts/getEpochServer.sh
+++ scripts/getEpochServer.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #downloads the epoch server files
 git clone https://github.com/EpochModTeam/Epoch.git /tmp/epoch_serv_src
-/bin/bash /scripts/makeLowerCase.sh /tmp/epoch_serv_src
+/bin/bash /src/scripts/makeLowerCase.sh /tmp/epoch_serv_src
 cp -r /tmp/epoch_serv_src/server_install_pack/* ${ARMA_DIR}
